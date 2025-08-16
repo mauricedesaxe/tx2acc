fa4b5c80f30ffc2e6e44ecfc747b816bbd4d541b
lazaralex98 <alex@leetsoftware.com>
2025-08-16 20:01:37 +0300

    feat: check if file exists

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 mod types;
 use std::collections::HashMap;
 use std::env;
+use std::path::Path;
 use types::{Client, ProcessedTransaction};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let input_file = if args.len() > 1 { &args[1] } else { "unknown" };
     eprintln!("input = {}", input_file);
 
+    if !Path::new(input_file).exists() {
+        eprintln!("Error: File '{}' not found", input_file);
+        return Ok(());
+    }
+
     let mut transactions: HashMap<u32, ProcessedTransaction> = HashMap::new();
     let mut clients: HashMap<u16, Client> = HashMap::new();
+
+    Ok(())
 }
