50174cf931c5b3c583441cad5e200f27ea8b550b
lazaralex98 <alex@leetsoftware.com>
2025-08-16 14:31:50 +0300

    feat: add Transaction struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use std::env;
 
+#[derive(Debug, Clone)]
+enum TransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+#[derive(Debug, Clone)]
+struct Transaction {
+    transaction_type: TransactionType,
+    client_id: u16,
+    transaction_id: u32,
+    amount: Option<u64>,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file = if args.len() > 1 { &args[1] } else { "unknown" };
