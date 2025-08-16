b9178374a19c8a3db682a6930bbcffc4779fbf2e
lazaralex98 <alex@leetsoftware.com>
2025-08-16 19:52:08 +0300

    feat: add ProcessedTransaction struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use std::env;
 
 #[derive(Debug, Clone)]
-enum TransactionType {
+enum RawTransactionType {
     Deposit,
     Withdrawal,
     Dispute,
@@ -10,8 +11,31 @@ enum TransactionType {
 }
 
 #[derive(Debug, Clone)]
-struct Transaction {
-    transaction_type: TransactionType,
+struct RawTransaction {
+    transaction_type: RawTransactionType,
+    client_id: u16,
+    transaction_id: u32,
+    amount: Option<u64>,
+}
+
+#[derive(Debug, Clone)]
+enum ProcessedTransactionType {
+    Deposit,
+    Withdrawal,
+}
+
+#[derive(Debug, Clone)]
+enum DisputeStatus {
+    Valid,
+    Disputed,
+    Resolved,
+    ChargedBack,
+}
+
+#[derive(Debug, Clone)]
+struct ProcessedTransaction {
+    transaction_type: ProcessedTransactionType,
+    dispute_status: DisputeStatus,
     client_id: u16,
     transaction_id: u32,
     amount: Option<u64>,
@@ -21,4 +45,5 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file = if args.len() > 1 { &args[1] } else { "unknown" };
     eprintln!("input = {}", input_file);
+
 }
